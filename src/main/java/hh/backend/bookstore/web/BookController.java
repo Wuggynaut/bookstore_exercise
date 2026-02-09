30d6746782c5e72b8515356cec3e0a22a43d3915
Wuggy <ari-matti.toivonen@myy.haaga-helia.fi>
2026-02-09 17:29:21 +0200

    Book list page

--- src/main/java/hh/backend/bookstore/web/BookController.java
+++ src/main/java/hh/backend/bookstore/web/BookController.java
@@ -1,13 +1,29 @@
 package hh.backend.bookstore.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import hh.backend.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
 
+    private BookRepository bookRepository;
+
+    // konstruktori-injektio
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     @GetMapping("/index")
     public String frontPage() {
         return "index";
+    }
+
+    @GetMapping("/booklist")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist";
     }
 }
