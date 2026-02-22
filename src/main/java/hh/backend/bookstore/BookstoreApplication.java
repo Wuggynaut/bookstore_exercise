ae67590c59c4d1bcd03c3d209a18039249308e56
Wuggy <ari-matti.toivonen@myy.haaga-helia.fi>
2026-02-22 15:52:24 +0200

    Category & logger

--- src/main/java/hh/backend/bookstore/BookstoreApplication.java
+++ src/main/java/hh/backend/bookstore/BookstoreApplication.java
@@ -1,5 +1,7 @@
 package hh.backend.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,20 +9,37 @@ import org.springframework.context.annotation.Bean;
 
 import hh.backend.bookstore.domain.Book;
 import hh.backend.bookstore.domain.BookRepository;
+import hh.backend.bookstore.domain.Category;
+import hh.backend.bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
 	@Bean
-	public CommandLineRunner exampleData(BookRepository bookRepository) {
+	public CommandLineRunner exampleData(BookRepository bookRepository, CategoryRepository categoryRepository) {
 		return (args) -> {
+			log.info("save example books and categories");
 			bookRepository.save(new Book("A Door Into Ocean", "Joan Slonczewski", 1986, "9780312876524", 17.85));
 			bookRepository.save(new Book("Alien Clay", "Adrian Tchaikovsky", 2024, "9780316578974", 17.25));
 			bookRepository.save(new Book("The Metamorphosis", "Franz Kafka", 1915, "9780553213690", 9.40));
+			categoryRepository.save(new Category("Science Fiction"));
+			categoryRepository.save(new Category("Literary Fiction"));
+			categoryRepository.save(new Category("Fantasy"));
+
+			log.info("fetch all books");
+			for (Book book : bookRepository.findAll()) {
+				log.info(book.toString());
+			}
+			log.info("fetch all categories");
+			for (Category category : categoryRepository.findAll()) {
+				log.info(category.toString());
+			}
 		};
 	}
 
